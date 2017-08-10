ec40425b46ec89ed1f1f87aabf4377e6157142c9
Jamanya <richardson.james@outlook.com>
2017-08-10 16:43:14 -0400

    added album list and basic styling

--- index.ios.js
+++ index.ios.js
@@ -2,9 +2,17 @@
 
 //import a library to help create a component 
 import React from 'react';
-import ReactNative from 'react-native';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 //create a component
-
+const App = () => (
+    <View>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+    </View>
+);
 
 //render it to the device 
+AppRegistry.registerComponent('albums', () => App);
