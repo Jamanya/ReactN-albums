b3f678d4de0429bc4a8722616824d029bac5ef3f
Jamanya <richardson.james@outlook.com>
2017-08-16 20:09:24 -0400

    added album artwork and scrollView

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -5,19 +5,28 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
 
-    const { title, artist, thumbnail_image } = album;
-    const { thumbnailStyle, headerContentStyle } = styles;
+    const { title, artist, thumbnail_image, image } = album;
+    const { 
+        thumbnailStyle, 
+        headerTextStyle, 
+        thumbnailContainerStyle, 
+        headerContentStyle,
+        imageStyle
+    } = styles;
 
     return (
         <Card>
             <CardSection>
-                <View>
+                <View style={thumbnailContainerStyle}>
                     <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
                 </View>
                 <View style={headerContentStyle}>
-                    <Text>{title}</Text>
+                    <Text style={headerTextStyle}>{title}</Text>
                     <Text>{artist}</Text>
                 </View>
+            </CardSection>
+            <CardSection>
+                <Image style={imageStyle} source={{ uri: image }} />
             </CardSection>
         </Card>
     );
@@ -28,9 +37,23 @@ const styles = {
         flexDirection: 'column',
         justifyContent: 'space-around'
     }, 
+    headerTextStyle: {
+        fontSize: 18
+    },
     thumbnailStyle: {
         height: 50,
         width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 10,
+        marginLeft: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 };
 
