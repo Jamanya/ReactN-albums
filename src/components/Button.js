71b4ca2ffd08794fb7ccdced736635c4e3ba097e
Jamanya <richardson.james@outlook.com>
2017-08-16 20:47:08 -0400

    added button and styles

---
+++ src/components/Button.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+const Button = () => {
+
+    const { buttonStyle, textStyle } = styles;
+
+    return (
+        <TouchableOpacity onPress={ () => console.log('Pressed')} style={buttonStyle}>
+            <Text style={textStyle}>Purchase</Text>
+        </TouchableOpacity>
+    );
+};
+
+const styles = {
+    textStyle: {
+        alignSelf: 'center',
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: '600',
+        paddingTop: 10,
+        paddingBottom: 10
+    },
+    buttonStyle: {
+        flex: 1,
+        alignSelf: 'stretch',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#fff',
+        backgroundColor: '#ff9900',
+        marginLeft: 5,
+        marginRight: 5,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 2
+
+    }
+};
+
+export default Button;
